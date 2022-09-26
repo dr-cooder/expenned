f230ebafa0fb9322fb245d49d315fd56ade75832
Felix Warner <fwarner2319@gmail.com>
2022-09-26 18:14:58 -0400

    Created boilerplate

---
+++ src/jsonResponses.js
@@ -0,0 +1,23 @@
+const respond = (request, response, status, responseJSON) => {
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+  });
+  if (responseJSON) response.write(JSON.stringify(responseJSON));
+  response.end();
+};
+
+const notFound = (request, response) => {
+  let responseJSON;
+  if (request.method !== 'HEAD') {
+    responseJSON = {
+      message: 'The page you are looking for was not found.',
+      id: 'notFound',
+    };
+  }
+
+  return respond(request, response, 404, responseJSON);
+};
+
+module.exports = {
+  notFound,
+}
